25fb9592ab94bb6e7a940fb5ade89e71f97b635a
Nima Cortez <cortez.nima@gmail.com>
2022-09-26 01:52:22 +0000

    changes to assertEqual changes to head and tail

--- head.js
+++ head.js
@@ -1,8 +1,15 @@
-const assertEqual = function(actual, expected) {
 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(head[5,6,7], 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"):
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
