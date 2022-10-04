477f095e17318ae87a23b7fc82a5817356cd6359
Nima Cortez <cortez.nima@gmail.com>
2022-10-04 01:50:21 +0000

    adding that file

---
+++ assertArraysEqual.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+      }
+    };
