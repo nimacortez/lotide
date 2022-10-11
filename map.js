1b5a54d94d83f9b972118fa4a5cef49efaa619de
Nima Cortez <cortez.nima@gmail.com>
2022-10-11 00:49:27 +0000

    changes to that file

--- map.js
+++ map.js
@@ -1,11 +1,3 @@
-const map = function(array, callback) {
-  const results = [];
-  for (let item of array) {
-    results.push(callback(item));
-  }
-  return results;
-}
-
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
@@ -13,3 +5,34 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
       }
     };
+
+    const eqArrays = function(array1, array2) {
+      if (array1.length !== array2.length) {
+        return false;
+      }
+      for (let i = 0; i < array1.length; i++) {
+        if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+          return false;
+        }
+        if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+          return false;
+        }
+      } 
+      return true;
+    };
+    
+
+    const map = function(array, callback) {
+      const results = [];
+      for (let item of array) {
+        results.push(callback(item));
+      }
+      return results;
+    };
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results = map(words, word => word[0]);
+assertArraysEqual(results, [ 'g', 'c', 't', 'm', 't' ]);
+
+const results2 = map(words, word => word.toUpperCase());
+assertArraysEqual(results2, [ 'GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM' ]);
