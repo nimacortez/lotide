71fc85f8eede253160e43ed208bc48d54bcd51d3
Nima Cortez <cortez.nima@gmail.com>
2022-10-20 21:57:27 +0000

    adding middle

---
+++ middle.js
@@ -0,0 +1,12 @@
+const middle = function(array) {
+  if (array.length < 3) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return array.slice(array.length / 2 - 1, array.length / 2 + 1)
+  } else {
+     let newArray = [];
+     let middleNum = Math.round(array.length /2)
+     newArray.push(array[middleNum -1]);
+     return newArray;
+  }
+};
