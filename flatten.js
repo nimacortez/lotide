3993f5b25d98b1fa360f0ae169f0b14f6db193f3
Nima Cortez <cortez.nima@gmail.com>
2022-10-25 03:52:12 +0000

    adding flatten

---
+++ flatten.js
@@ -0,0 +1,40 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+      return false;
+    }
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+      return false;
+    }
+  } 
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+      }
+    };
+
+const flatten = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let f = 0; f < array[i].length; f++) {
+        newArray.push(array[i][f]);
+      }
+    } else { 
+      newArray.push(array[i]);
+  }
+}
+  return newArray; 
+};
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+
+module.exports = flatten;
