5de0f4b9ff1f7c36b506e64cf6785c0c9cfaa21e
Nima Cortez <cortez.nima@gmail.com>
2022-11-14 23:50:17 +0000

    fixed errors

--- eqObjects.js
+++ eqObjects.js
@@ -1,40 +1,39 @@
-const eqArrays = require('./eqArrays');
+const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function(actual, expected) {
+const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-      }
-    };
+  }
+};
 
-
-
-  const eqObjects = function(object1, object2) {
-    let object1Keys = Object.keys(object1);
-    let object2Keys = Object.keys(object2);
-    if (object1Keys.length !== object2Keys.length) {
-      return false;
-    } else {
+const eqObjects = function (object1, object2) {
+  let object1Keys = Object.keys(object1);
+  let object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  } else {
     for (const key of object1Keys) {
       if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) {
           return false;
         }
-      } else if
-        (typeof object1[key] === "object" && typeof object2[key] === "object")
-      if (!eqObjects(object1[key], object2[key])) {
-        return false;
+      } else if (
+        typeof object1[key] === "object" &&
+        typeof object2[key] === "object"
+      ) {
+        if (!eqObjects(object1[key], object2[key])) {
+          return false;
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
       }
-    } else { 
-      if (object1[key] !== object2[key]);
-      {
-      return false;
     }
+    return true;
   }
-  } 
-  return true; 
-    };
-  
+};
 
 module.exports = eqObjects;
--- letterPositions.js
+++ letterPositions.js
@@ -1,32 +1,31 @@
-const assertArraysEqual = function(actual, expected) {
+const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-      }
-    };
+  }
+};
 
-    const eqArrays = function(array1, array2) {
-      if (array1.length !== array2.length) {
-        return false;
-      }
-      for (let i = 0; i < array1.length; i++) {
-        if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
-          return false;
-        }
-        if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
-          return false;
-        }
-      } 
-      return true;
-    };
-    
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && eqArrays(array1[i], array2[i]) === false) {
+      return false;
+    }
+    if (!Array.isArray(array1[i]) && array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
     let letter = sentence[i];
-    if (letter === ' ') {
+    if (letter === " ") {
       continue;
     } else if (!results[letter]) {
       results[letter] = [i];
@@ -35,6 +34,6 @@ const letterPositions = function(sentence) {
     }
   }
   return results;
-}; 
+};
 
- module.exports = findKeyByValue;
+module.exports = letterPositions;
